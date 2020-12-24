46a55086f46eae4c3a5ae5371b0f44d2f44a183f
Hakan Bali <h.bali@hymnai.com>
2020-12-24 18:12:56 +0000

    Utility Class introduced for constants

---
+++ src/main/java/com/arkhelais/groceryscraper/util/Constants.java
@@ -0,0 +1,13 @@
+package com.arkhelais.groceryscraper.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+  public static final String DEFAULT_URL =
+      "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+  public static final String CSS_PRODUCT = "productNameAndPromotions";
+  public static final Integer VAT = 20;   // Percentage
+
+}
