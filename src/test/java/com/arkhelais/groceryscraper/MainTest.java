3875ca940950b3bf3cc536da425cb295347a10a9
Hakan Bali <h.bali@hymnai.com>
2020-12-26 06:59:25 +0000

    Unit Test and regarding utility class is introduced.

---
+++ src/test/java/com/arkhelais/groceryscraper/MainTest.java
@@ -0,0 +1,47 @@
+package com.arkhelais.groceryscraper;
+
+import static com.arkhelais.groceryscraper.util.TestConstants.GROSS;
+import static com.arkhelais.groceryscraper.util.TestConstants.KCAL;
+import static com.arkhelais.groceryscraper.util.TestConstants.OUTPUT_SIZE;
+import static com.arkhelais.groceryscraper.util.TestConstants.TITLE;
+import static com.arkhelais.groceryscraper.util.TestConstants.VAT;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.arkhelais.groceryscraper.dto.Output;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+  private final PrintStream standardOut = System.out;
+
+  @BeforeEach
+  void setUp() {
+    System.setOut(new PrintStream(outputStreamCaptor));
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.setOut(standardOut);
+  }
+
+  @Test
+  void main() throws JsonProcessingException {
+    Main.main(null);
+
+    Output output = new ObjectMapper().readValue(outputStreamCaptor.toString().trim(), Output.class);
+    assertEquals(GROSS, output.getTotal().getGross());
+    assertEquals(VAT, output.getTotal().getVat());
+    assertEquals(OUTPUT_SIZE, output.getResults().size());
+    assertEquals(KCAL, output.getResults().get(10).getKcal());
+    assertEquals(TITLE, output.getResults().get(15).getTitle());
+    assertNull(output.getResults().get(16).getKcal());
+  }
+
+}
---
+++ src/test/java/com/arkhelais/groceryscraper/util/TestConstants.java
@@ -0,0 +1,14 @@
+package com.arkhelais.groceryscraper.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestConstants {
+
+  public static final Double GROSS = 39.5;
+  public static final Double VAT = 6.58;
+  public static final int OUTPUT_SIZE = 17;
+  public static final int KCAL = 52;
+  public static final String TITLE = "Sainsbury's Blackcurrants 150g";
+
+}
