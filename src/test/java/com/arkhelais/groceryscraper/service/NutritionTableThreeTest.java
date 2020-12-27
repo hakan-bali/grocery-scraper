cb3f6978069e608005d6f7de649b96505de11860
Hakan Bali <h.bali@hymnai.com>
2020-12-27 17:16:14 +0000

    REFACTOR: Changed due to naming convention.

--- src/test/java/com/arkhelais/groceryscraper/service/NutritionTableThreeTest.java
+++ src/test/java/com/arkhelais/groceryscraper/service/NutritionTableThreeTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class NutritionTableTwoThree {
+class NutritionTableThreeTest {
 
   private final NutritionTableOne nutritionTableOne =
       new NutritionTableOne(
